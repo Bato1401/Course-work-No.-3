f5547ca0c9dc5e3ea8f5ef616365ea48fedce89b
Bato1401 <lego.1401@gmail.com>
2023-04-02 19:04:57 +0300

    In utils.py created a functions get_data_output(), to output the required information

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from utils import get_data, get_filtered_data, get_last_values
+from utils import get_data, get_filtered_data, get_last_values, get_data_output
 
 
 def main():
     data = get_data()
     data = get_filtered_data(data)
     data = get_last_values(data)
+    data = get_data_output(data)
+    for row in data:
+        print(row, end='\n\n')
+
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -9,6 +9,7 @@ def get_data():
         data = json.load(file)
     return data
 
+
 def get_filtered_data(data):
     """
     Функция фильтрует данные по заданным параметрам и сохраняет в переменную data
@@ -22,3 +23,23 @@ def get_last_values(data):
     Функция сортирует отфильтрованные данные по дате и выстраивает на убывание
     """
     data = sorted(data, key=lambda x: x['date'], reverse=True)
+    return data[:5]  # Возвращает 5 отсортированных транзакций
+
+
+def get_data_output(data):
+    """
+    Функция выводит данные в требуемом формате
+    """
+    operations = []
+    for row in data:
+        date_ = row["date"]
+        first_line = f'{date_[8:10]}.{date_[5:7]}.{date_[:4]} {row["description"]}'  # первая строка
+        card_name = ''.join([x for x in row['from'] if x.isalpha()])
+        card_num = ''.join([x for x in row['from'] if x.isdigit()])
+        account_name = ''.join([x for x in row['to'] if x.isalpha()])
+        account_num = ''.join([x for x in row['to'] if x.isdigit()])
+        second_line = f'{card_name} {card_num[:4]} {card_num[4:6]}** **** {card_num[-4:]} ' \
+                      f'- > {account_name} **{account_num[-4:]}'  # вторая строка
+        third_line = f'{row["operationAmount"]["amount"]} {row["operationAmount"]["currency"]["name"]}'  # третья строка
+        operations.append(f'''{first_line}\n{second_line}\n{third_line}''')
+    return operations
